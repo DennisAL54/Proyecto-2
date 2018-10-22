b3407dcdb4e59e1b5b45a504b58f887e8fb34bd3
Unknown <alejimenezc@hotmail.com>
2018-10-22 17:31:21 -0600

    Insertion sort v1

--- Game/Oleada.java
+++ Game/Oleada.java
@@ -26,6 +26,7 @@ public class Oleada {
 
     public void SelectionSortEdad(){
 
+
     }
 
 
---
+++ src/GameofSorts/Nodo.java
@@ -0,0 +1,11 @@
+package GameofSorts;
+
+public class Nodo {
+    int Data;
+    Nodo siguiente;
+    public Nodo(int dato){
+        this.Data = Data;
+        this.siguiente = null;
+    }
+
+}
