e4c944de3c4ef1ab1f6c118628be732939d8ae28
bryanbarb02 <37256420+bryanbarb02@users.noreply.github.com>
2018-10-31 14:31:19 -0600

    Update Main.java

--- src/GameofSorts/Main.java
+++ src/GameofSorts/Main.java
@@ -6,6 +6,11 @@ import javax.swing.JFrame;
  * @author Bryan
  */
 public class Main extends JFrame {
+    public static int nivel = 1; //Max 7                            //Pasarian 126 cambios de alineacion
+    public static int cantDragones = 10; //Max 30
+    public static CreadorDeOleadas creador = new CreadorDeOleadas();
+    public static Lista oleada;
+    
     /**
      * Constructor de la ventana de juego
      */
@@ -13,7 +18,7 @@ public class Main extends JFrame {
         add(new Ventana());
         setTitle("Game of Sorts");
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setSize(950,600);
+        setSize(1350,645);  //950, 600
         setLocationRelativeTo(null);
         setVisible(true);
         setResizable(false);
@@ -21,6 +26,22 @@ public class Main extends JFrame {
 
     public static void main(String[] args) {
         new Main();
+    }
+    
+    public static int getNivel() {
+        return nivel;
+    }
+
+    public static void setNivel(int nivel) {
+        Main.nivel = nivel;
+    }
+
+    public static int getCantDragones() {
+        return cantDragones;
+    }
+
+    public static void setDragones(int cantDragones) {
+        Main.cantDragones = cantDragones;
     }
 
 }
