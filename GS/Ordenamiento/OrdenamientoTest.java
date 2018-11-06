438311f88178bf5326650e58d276b17780e6a391
Rubén Salas <rubsalas@hotmail.com>
2018-11-05 23:20:47 -0600

    JUnit Ordenamiento Test

---
+++ GS/Ordenamiento/OrdenamientoTest.java
@@ -0,0 +1,24 @@
+package Ordenamiento;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+
+public class OrdenamientoTest {
+
+    AVLTree avl;
+    BinaryTree binary;
+    BTree b;
+    CreadorDeOleadas creador;
+    Lista lista;
+
+    @BeforeAll
+    public void before(){
+        System.out.println();
+    }
+
+    @AfterAll
+    public void after(){
+        System.out.println();
+    }
+
+}
