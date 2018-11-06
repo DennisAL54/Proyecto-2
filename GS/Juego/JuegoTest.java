bd4f51853fe510b1f6081c04d0b1cd0b19308916
Rubén Salas <rubsalas@hotmail.com>
2018-11-05 23:20:16 -0600

    JUnit JuegoTest

---
+++ GS/Juego/JuegoTest.java
@@ -0,0 +1,25 @@
+package Juego;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+
+public class JuegoTest {
+
+    CaballeroMedieval caballero;
+    Dragon dragon;
+    Fuego fuego;
+    Layout layout;
+    Main main;
+    Ventana ventana;
+
+    @BeforeAll
+    public void before(){
+        System.out.println();
+    }
+
+    @AfterAll
+    public void after(){
+        System.out.println();
+    }
+
+}
